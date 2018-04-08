5128c3f1dfb3a5a710ac8b91daa0b25bace03b87
GovindChaudhary <govindchaudhary2404@gmail.com>
2018-04-08 11:30:31 +0530

    updated the ANN

--- ann.py
+++ ann.py
@@ -63,13 +63,33 @@ classifier.add(Dense(output_dim=6,init='uniform',activation='relu'))
 classifier.add(Dense(output_dim=1,init='uniform',activation='sigmoid'))
 
 #compiling the ANN
+#basically applying the stocashtic gradient descent on the whole ANN
+# optimizer:algorithm you want to use to find the optimal set ofweights in our neural network
+# we are using stocashtic gradient algo to optomize the set of weights
+#adam is a special type of stocashtic gradient algo
+# loss is the cost funcion within the stocashtic gradient algo and basically we have to optimize this func using stocashtic gradient algo to find the optimal set of weights
+# for eg loss func in linear regression is sum of squared differnce
+#in this case our cost func or loss func is same as logistic regression
+#since we are using a simple neural network with only one neuron and we are using sigmoid func for the output layer
+# so we will use loss func same as logistic regression which is lograthmic loss func denoted by crossentropy
+# further if we are dealing with binary outcome or dependent varaible we use binary_crossentropy
+# and for more than two we use categorical_crossentropy
+#finally matrices is the criterion you want to use to evaluate your model
+#our algo will use the accuracy criterin to improve our model
+
 classifier.compile(optimizer='adam' , loss='binary_crossentropy' ,metrics=['accuracy'])
 
 
 
 
 
-# Fitting ANN to the Training set
+
+# Fitting ANN to the Training set(conneting our ann model to training set
+#batch size is the no. of observation after which you want to optimze your weights
+#basically we have two options either to update the weights after each observation(reinforcement learning)
+#or to update it after a batch of observations (batch learning)
+# an epoch is basically a round when the whole training set is passed through the ann
+#nb_epoch is here the no. of epochs(based on practice)
 
 classifier.fit(X_train,y_train,batch_size=10,nb_epoch=100)
 
@@ -77,7 +97,7 @@ classifier.fit(X_train,y_train,batch_size=10,nb_epoch=100)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-y_pred = (y_pred>.5) #true if y_pred > .5 otherwise false
+y_pred = (y_pred>.5) #true if y_pred > .5 otherwise false basically choosing threshold = 0.5
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
