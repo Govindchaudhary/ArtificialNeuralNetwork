a921b4ff6a1b431229a2e42f225048fad40deac5
GovindChaudhary <govindchaudhary2404@gmail.com>
2018-04-08 16:29:35 +0530

    predicting for an individual customer

--- ann.py
+++ ann.py
@@ -99,6 +99,11 @@ y_pred = classifier.predict(X_test)
 
 y_pred = (y_pred>.5) #true if y_pred > .5 otherwise false basically choosing threshold = 0.5
 
+#classifier.predict expects a 2d array as input 
+#so here we are making our data as a 2d array but with only one row entry(France,600,male,40,3,60000,2,1,1,50000)
+new_prediction = classifier.predict(sc.transform(np.array([[0,0,600,1,40,3,60000,2,1,1,50000]])))
+new_prediction = (new_prediction>.5)
+
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
